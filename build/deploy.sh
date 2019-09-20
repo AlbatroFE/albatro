3926590323e03f71a9552ebbc0d7da56dbaa2952
GavinHome <yangxiaoming512@yeah.net>
2019-09-20 14:47:14 +0800

    update

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,20 +1,22 @@
 #! /bin/sh
 mkdir temp_web
-npm run deploy:build
+#npm run deploy:build
 cd temp_web
 git clone --depth 1 -b gh-pages --single-branch https://github.com/AlbatroFE/albatro.git && cd albatro
+# mock: mkdir albatro && cd albatro
 
 # build sub folder
-# cd ../$website
 SUB_FOLDER='0.1'
 mkdir $SUB_FOLDER
 rm -rf *.js *.css *.map static css fonts img js *.html *.ico *.json
 rm -rf $SUB_FOLDER/**
 cp -rf ../../dist/** .
 cp -rf ../../dist/** $SUB_FOLDER/
-cd ../..
+cd ../../..
 
-GH_PAGES_FOLDER='../gh-pages/'
+GH_PAGES_FOLDER='gh-pages'
+# mock: mkdir $GH_PAGES_FOLDER
+
 rm -rf $GH_PAGES_FOLDER/*.js 
 rm -rf $GH_PAGES_FOLDER/*.css 
 rm -rf $GH_PAGES_FOLDER/*.map 
@@ -23,16 +25,20 @@ rm -rf $GH_PAGES_FOLDER/*.json
 rm -rf $GH_PAGES_FOLDER/*.ico
 rm -rf $GH_PAGES_FOLDER/CNAME
 rm -rf $GH_PAGES_FOLDER/static
+rm -rf $GH_PAGES_FOLDER/css
+rm -rf $GH_PAGES_FOLDER/fonts
+rm -rf $GH_PAGES_FOLDER/img
+rm -rf $GH_PAGES_FOLDER/js
 rm -rf $GH_PAGES_FOLDER/$SUB_FOLDER
-cp -rf ./temp_web/albatro/** $GH_PAGES_FOLDER/
+
+cp -rf ./test/temp_web/albatro/** $GH_PAGES_FOLDER/
 
 # deploy domestic site
-# faas deploy alpha
-rm -rf temp_web
+# rm -rf ./test/temp_web
 
 echo "deploy gh-pages $SUB_FOLDER"
 cd $GH_PAGES_FOLDER
-# git add .
-# git commit -m "deploy gh-pages $SUB_FOLDER"
-# git push albatro gh-pages
+git add .
+git commit -m "deploy gh-pages $SUB_FOLDER"
+git push albatro gh-pages
 echo "deploy successful version: $SUB_FOLDER"
