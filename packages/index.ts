d103ee1bc9da1c389566ad140377847eec77b992
GavinHome <yangxiaoming512@yeah.net>
2019-10-11 17:27:47 +0800

    update

--- packages/index.ts
+++ packages/index.ts
@@ -49,7 +49,7 @@ const install = function(vue: any, opts: any = {}) {
 };
 
 export default {
-  version: "0.1.0",
+  version: "0.1.1",
   locale: locale.use,
   i18n: locale.i18n,
   install,
--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '0.1.0',
+  version: '0.1.1',
   locale: locale.use,
   i18n: locale.i18n,
   install,
